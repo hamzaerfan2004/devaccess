8dfbc05651b22d765747f9377a27a2d05183d196
Hamza Erfan <wmis222@live.rhul.ac.uk>
2026-09-15 17:17:30 +0100

    feat: initialize Spring Boot backend

---
+++ backend/src/main/java/com/hamzaerfan/devaccess/DevaccessBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hamzaerfan.devaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevaccessBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevaccessBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hamzaerfan/devaccess/DevaccessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamzaerfan.devaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevaccessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
